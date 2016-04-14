4c414275dc649e3480cccd3115af4f7583d8b7b8
dtracers <dtracers@gmail.com>
2016-04-13 23:06:50 -0500

    Added a basic sketch interface that can be used by the recognition server

---
+++ src/main/java/coursesketch/recognition/framework/RecognitionInterface.java
@@ -0,0 +1,69 @@
+package coursesketch.recognition.framework;
+
+import protobuf.srl.commands.Commands;
+import protobuf.srl.sketch.Sketch;
+
+/**
+ * Created by David Windows on 4/13/2016.
+ */
+public interface RecognitionInterface {
+    /**
+     * Adds a single
+     * @param sketchId The Id that uniquely identifies a single sketch.
+     * @param update a single update that is being added.
+     */
+    public void addUpdate(String sketchId, Commands.SrlUpdate update);
+
+    /**
+     *
+     * @param sketchId The Id that uniquely identifies a single sketch.
+     * @param updateList The update list being set.
+     */
+    public void setUpdateList(String sketchId, Commands.SrlUpdateList updateList);
+
+    /**
+     *
+     * @param sketchId The Id that uniquely identifies a single sketch.
+     * @param sketch The sketch being set.
+     */
+    public void setSketch(String sketchId, Sketch sketch);
+
+    /**
+     * Adds a template to be saved for use in recognition later.
+     *
+     * @param template The template that is being saved.
+     */
+    public void addTemplate(Sketch template);
+
+    /**
+     * Adds a template to be saved for use in recognition later.
+     *
+     * @param template The template that is being saved.
+     */
+    public void addTemplate(Sketch.SrlShape template);
+
+    /**
+     * Adds a template to be saved for use in recognition later.
+     *
+     * @param template The template that is being saved.
+     */
+    public void addTemplate(Sketch.SrlStroke template);
+
+    /**
+     * Recognizes the sketch as a list of changes producing a list of changes as a result.
+     *
+     * @param sketchId The Id that uniquely identifies a single sketch.
+     * @param updateList The list that is being recognized.
+     * @return A list representing the changes that occurred.
+     */
+    public Commands.SrlUpdateList recognize(String sketchId, Commands.SrlUpdateList updateList);
+
+    /**
+     * Recognizes the sketch as a single entity producing a recognized sketch as a result.
+     *
+     * @param sketchId The Id that uniquely identifies a single sketch.
+     * @param sketch The sketch that is being recognized.
+     * @return A sketch representing the recognized result.
+     */
+    public Sketch recognize(String sketchId, Sketch sketch);
+}
