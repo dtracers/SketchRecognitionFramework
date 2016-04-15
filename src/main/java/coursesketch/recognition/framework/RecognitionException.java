4814de541acb56fbae9cd9cdb3f2410dd2240b56
dtracers <dtracers@gmail.com>
2016-04-14 23:27:46 -0500

    Added a recognition exception

---
+++ src/main/java/coursesketch/recognition/framework/RecognitionException.java
@@ -0,0 +1,26 @@
+package coursesketch.recognition.framework;
+
+/**
+ * Created by David Windows on 4/14/2016.
+ */
+public class RecognitionException extends Exception {
+
+    /**
+     * Creates an exception with a message.
+     *
+     * @param message The message of the exception.
+     */
+    public RecognitionException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Creates an exception with a cause.
+     *
+     * @param message The message of the exception.
+     * @param cause
+     */
+    public RecognitionException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
