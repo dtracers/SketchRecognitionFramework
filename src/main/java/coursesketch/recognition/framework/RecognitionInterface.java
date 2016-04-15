1c96a0f461f985c1b1154d846dcb2323ca29dc0d
dtracers <dtracers@gmail.com>
2016-04-14 23:42:10 -0500

    Added more exceptions

--- src/main/java/coursesketch/recognition/framework/RecognitionInterface.java
+++ src/main/java/coursesketch/recognition/framework/RecognitionInterface.java
@@ -1,5 +1,7 @@
 package coursesketch.recognition.framework;
 
+import coursesketch.recognition.framework.exceptions.RecognitionException;
+import coursesketch.recognition.framework.exceptions.TemplateException;
 import protobuf.srl.commands.Commands;
 import protobuf.srl.sketch.Sketch;
 import protobuf.srl.sketch.Sketch.SrlSketch;
@@ -12,43 +14,49 @@ public interface RecognitionInterface {
      * Adds a single
      * @param sketchId The Id that uniquely identifies a single sketch.
      * @param update a single update that is being added.
+     * @throws RecognitionException Thrown if there is a recognition Problem.
      */
-    public void addUpdate(String sketchId, Commands.SrlUpdate update);
+    public void addUpdate(String sketchId, Commands.SrlUpdate update) throws RecognitionException;
 
     /**
      *
      * @param sketchId The Id that uniquely identifies a single sketch.
      * @param updateList The update list being set.
+     * @throws RecognitionException Thrown if there is a recognition Problem.
      */
-    public void setUpdateList(String sketchId, Commands.SrlUpdateList updateList);
+    public void setUpdateList(String sketchId, Commands.SrlUpdateList updateList) throws RecognitionException;
 
     /**
      *
      * @param sketchId The Id that uniquely identifies a single sketch.
      * @param sketch The sketch being set.
+     * @throws RecognitionException Thrown if there is a recognition Problem.
      */
-    public void setSketch(String sketchId, SrlSketch sketch);
+    public void setSketch(String sketchId, SrlSketch sketch) throws RecognitionException;
 
     /**
      * Adds a template to be saved for use in recognition later.
      *
      * @param template The template that is being saved.
+     * @throws TemplateException Thrown if there is a recognition Problem.
      */
-    public void addTemplate(SrlSketch template);
+    public void addTemplate(SrlSketch template) throws TemplateException;
 
     /**
      * Adds a template to be saved for use in recognition later.
      *
      * @param template The template that is being saved.
+     * @throws TemplateException Thrown if there is a recognition Problem.
      */
-    public void addTemplate(Sketch.SrlShape template);
+    public void addTemplate(Sketch.SrlShape template) throws TemplateException;
 
     /**
      * Adds a template to be saved for use in recognition later.
      *
      * @param template The template that is being saved.
+     * @throws TemplateException Thrown if there is a recognition Problem.
      */
-    public void addTemplate(Sketch.SrlStroke template);
+    public void addTemplate(Sketch.SrlStroke template) throws TemplateException;
 
     /**
      * Recognizes the sketch as a list of changes producing a list of changes as a result.
@@ -56,8 +64,9 @@ public interface RecognitionInterface {
      * @param sketchId The Id that uniquely identifies a single sketch.
      * @param updateList The list that is being recognized.
      * @return A list representing the changes that occurred.
+     * @throws RecognitionException Thrown if there is a recognition Problem.
      */
-    public Commands.SrlUpdateList recognize(String sketchId, Commands.SrlUpdateList updateList);
+    public Commands.SrlUpdateList recognize(String sketchId, Commands.SrlUpdateList updateList) throws RecognitionException;;
 
     /**
      * Recognizes the sketch as a single entity producing a recognized sketch as a result.
@@ -65,6 +74,7 @@ public interface RecognitionInterface {
      * @param sketchId The Id that uniquely identifies a single sketch.
      * @param sketch The sketch that is being recognized.
      * @return A sketch representing the recognized result.
+     * @throws RecognitionException Thrown if there is a recognition Problem.
      */
-    public SrlSketch recognize(String sketchId, SrlSketch sketch);
+    public SrlSketch recognize(String sketchId, SrlSketch sketch) throws RecognitionException;;
 }
--- src/main/java/coursesketch/recognition/framework/exceptions/TemplateException.java
+++ src/main/java/coursesketch/recognition/framework/exceptions/TemplateException.java
@@ -1,16 +1,18 @@
-package coursesketch.recognition.framework;
+package coursesketch.recognition.framework.exceptions;
 
 /**
+ * An exception when something goes wrong with templates.
+ *
  * Created by David Windows on 4/14/2016.
  */
-public class RecognitionException extends Exception {
+public class TemplateException extends RecognitionException {
 
     /**
      * Creates an exception with a message.
      *
      * @param message The message of the exception.
      */
-    public RecognitionException(final String message) {
+    public TemplateException(final String message) {
         super(message);
     }
 
@@ -20,7 +22,7 @@ public class RecognitionException extends Exception {
      * @param message The message of the exception.
      * @param cause
      */
-    public RecognitionException(final String message, final Throwable cause) {
+    public TemplateException(final String message, final Throwable cause) {
         super(message, cause);
     }
 }
