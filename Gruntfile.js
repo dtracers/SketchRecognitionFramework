3ec0b66c62c97581cce1cc43a2a12505e00337d3
dtracers <dtracers@gmail.com>
2016-05-17 13:16:35 -0600

    attempting to add ability to build js proto files instead of source proto files

---
+++ Gruntfile.js
@@ -0,0 +1,310 @@
+//jscs:disable jsDoc
+
+var rewriteRulesSnippet = require('grunt-connect-rewrite/lib/utils').rewriteRequest;
+var selenium = require('selenium-standalone');
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-regex-check');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadTasks('config/gruntTasks/');
+
+    var gruntOptions = {
+        skipTests: (process.env.GRUNT_SKIP_TESTS || false) === 'true'
+    };
+
+    console.log(gruntOptions);
+
+    /******************************************
+     * GRUNT INIT
+     ******************************************/
+
+    grunt.initConfig({
+        fileConfigOptions: {
+            prodHtml: [ 'target/website/index.html', 'target/website/src/**/*.html', '!target/website/src/main/web/utilities/libraries/**/*.html' ],
+            prodFiles: [ 'target/website/index.html', 'target/website/src/**/*.html', 'target/website/src/**/*.js',
+                '!target/website/src/main/web/utilities/libraries/**/*.js', '!target/website/src/main/web/utilities/libraries/**/*.html' ]
+        },
+        /**
+         * CHECKSTYLE
+         */
+        jshint: {
+            options: {
+                jshintrc: 'config/.jshintrc',
+                ignores: [ 'src/main/web/utilities/libraries/**/*.js', 'src/test/web/testUtilities/**/*.js' ],
+                globals: {
+                    module: true
+                },
+                reporter:'jslint',
+                reporterOutput: 'target/jshint.xml'
+            },
+            files: [ 'Gruntfile.js', 'src/main/web/**/*.js', 'src/test/web/**/*.js',
+                '!src/main/web/utilities/libraries/**/*.js', '!src/test/web/**/*.js', '!src/main/web/sketching/srl/objects/**/*.js' ]
+        },
+        jscs: {
+            src: '<%= jshint.files %>',
+            options: {
+                config: 'config/jscs.conf.jscsrc',
+                reporterOutput: 'target/jscsReport.txt',
+                maxErrors: 1000
+            }
+        },
+        /*
+         * This module is used to check the existence or the lack of existance of a pattern in the given files
+         */
+        'regex-check': {
+            head: {
+                files: {
+                    src: [ 'src/main/web/**/*.html', 'src/test/web/**/*.html', '!src/main/web/utilities/libraries/**/*.html',
+                        'src/main/web/utilities/libraries/**/*Include.html' ]
+                },
+                options: {
+                    // This looks for the head tag <head>
+                    pattern: /<head(\s|(.*lang=.*))*>/g,
+                    failIfMissing: true
+                }
+            }
+        },
+        /**
+         * JSDOC
+         */
+        jsdoc: {
+            dist: {
+                src: '<%= jshint.files %>',
+                options: {
+                    destination: 'doc'
+                }
+            }
+        },
+        /**
+         * Directory Creation
+         */
+        mkdir: {
+            all: {
+                options: {
+                    create: [ 'target/unitTest', 'target/screenshots' ]
+                }
+            }
+        },
+        /**
+         * BUILDERS
+         */
+        babel: {
+            options: {
+                sourceMap: true
+            },
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: [ 'target/website/src/main/web/**/*.js', '!target/website/src/main/web/utilities/libraries/**/*.js' ],
+                        dest: '.'
+                    }
+                ]
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        // copies the website files used in production for prod use
+                        expand: true,
+                        src: [ 'src/**', '!src/test/**',
+                            // we do not want these copied as they are legacy.
+                            '!src/html/**', '!src/js/**',
+                            // we do not want these copied as they are not meant for production.
+                            '!src/**/debug/**' ],
+
+                        dest: 'target/website/'
+                    },
+                    {
+                        // copies other important files that appear in the top level directory
+                        expand: true,
+                        src: [ 'index.html', 'favicon.ico', 'bower.json' ],
+                        dest: 'target/website/'
+                    },
+                    {
+                        // copies the bower components to target
+                        expand: true,
+                        src: [ 'bower_components/**', '!bower_components/**/test/**', '!bower_components/**/tests/**',
+                            '!bower_components/**/examples/**' ],
+                        dest: 'target/website/'
+                    },
+                    {
+                        // copies the google app engine directory file
+                        expand: true,
+                        src: 'app.yaml',
+                        dest: 'target/website/'
+                    },
+                    {
+                        // copies the rest of the google app engine files
+                        expand: true,
+                        src: [ 'testFiles.py', 'main.py' ],
+                        dest: 'target/website/'
+                    }
+                ]
+            },
+            /**
+             * copies the babel polyfill into the bower_components folder
+             */
+            babel: {
+                files: [
+                    {
+                        expand: false,
+                        src: [ 'node_modules/babel-core/browser-polyfill.js' ],
+                        dest: 'bower_components/babel-polyfill/browser-polyfill.js',
+                        filter: 'isFile'
+                    },
+                    {
+                        expand: false,
+                        src: [ 'bower_components/babel-polyfill/.bower.json' ],
+                        dest: 'bower_components/babel-polyfill/bower.json',
+                        filter: 'isFile'
+                    }
+                ]
+            }
+        },
+        replace: {
+            bowerLoad: {
+                src: '<%= fileConfigOptions.prodHtml %>',
+                overwrite: true,
+                replacements: [
+                    {
+                        // looks for <head>
+                        from: /(^|\s)<head>($|\s)/g,
+                        to: '\n<head>\n<!-- bower:js -->\n<!-- endbower -->\n'
+                    }
+                ]
+            },
+            bowerSlash: {
+                src: '<%= fileConfigOptions.prodHtml %>',
+                overwrite: true,
+                replacements: [
+                    {
+                        // looks for the bower_components url in scripts and replaces it with a /
+                        from: /=['"].*bower_components/g,
+                        to: '="/bower_components'
+                    }
+                ]
+            },
+            isUndefined: {
+                src: '<%= fileConfigOptions.prodFiles %>',
+                overwrite: true,
+                replacements: [
+                    {
+                        // looks for isUndefined(word).
+                        from: /isUndefined\((\w+\b)\)/g,
+                        to: '(typeof $1 === \'undefined\')'
+                    },
+                    {
+                        from: 'function (typeof object === \'undefined\')',
+                        to: 'function isUndefined(object)'
+                    }
+                ]
+            }
+        },
+    });
+
+    /******************************************
+     * UTILITIES
+     ******************************************/
+
+    function printTaskGroup() {
+        grunt.log.write('\n===========\n=========== Running task group ' + grunt.task.current.name + ' ===========\n===========\n');
+    }
+
+    /******************************************
+     * TASK WORKFLOW SETUP
+     ******************************************/
+
+    // sets up tasks relating to starting the server
+    grunt.registerTask('server', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'seleniumStandalone:run',
+            'configureRewriteRules',
+            'connect:development'
+        ]);
+    });
+
+    // sets up tasks related to creating documentation
+    grunt.registerTask('documentation', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'jsdoc'
+        ]);
+    });
+
+    // Sets up tasks related to setting the system for the rest of the tasks.
+    grunt.registerTask('setup', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'mkdir'
+        ]);
+    });
+
+    // sets up tasks related to checkstyle
+    grunt.registerTask('checkstyle', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'jscs',
+            'jshint',
+            'regex-check'
+        ]);
+    });
+
+    // sets up tasks related to building the production website
+    grunt.registerTask('build', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'preBuild',
+            'setupProd',
+            'bower',
+            'polyfill'
+        ]);
+    });
+
+    // Sets up tasks related to setting up the production website.
+    grunt.registerTask('setupProd', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'copy:main',
+        ]);
+    });
+
+    // sets up tasks related to loading up bower
+    grunt.registerTask('bower', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'replace:bowerLoad',
+            'wiredep',
+            'replace:bowerRunOnce',
+            'replace:bowerSlash',
+            'replace:runOncePlugins'
+        ]);
+    });
+
+    // sets up tasks related to supporting older version of browsers
+    grunt.registerTask('polyfill', function() {
+        printTaskGroup();
+        grunt.task.run([
+            'replace:isUndefined'
+            // babel is turned off because it is breaking things.
+            //'babel'
+        ]);
+    });
+
+    /******************************************
+     * TASK WORKFLOW RUNNING
+     ******************************************/
+
+    // 'test'  wait till browsers are better supported
+    grunt.registerTask('default', [ 'checkstyle', 'documentation', 'setup', 'build' ]);
+};
